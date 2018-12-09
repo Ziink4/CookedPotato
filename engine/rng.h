db405e16f24a2a462588c44262c9662658a70e13
Florian B <deamond4rk@gmail.com>
2018-12-09 21:10:33 +0100

    RNG Cleanup

--- engine/rng.h
+++ engine/rng.h
@@ -8,33 +8,28 @@ namespace engine
 class RandomNumberGenerator
 {
 private:
-	using generator = std::mt19937;
+    using generator = std::mt19937;
 
-	generator::result_type m_seed;
+    generator::result_type m_seed;
 
-	generator m_rng;
+    generator m_rng;
 
 public:
-	using seed_type = generator::result_type;
-	/// the default seed produces a different set of random values on each run
-	static constexpr seed_type default_seed = 0;
-	/// the fixed seed is set to the generator default seed
-	static constexpr seed_type fixed_seed = generator::default_seed;
+    using seed_type = generator::result_type;
 
-	explicit RandomNumberGenerator(seed_type seed = default_seed) noexcept : m_seed(seed)
-	{
-		/// a seed of 0 will randomly create a seed
-		if (m_seed == default_seed)
-		{
-			std::random_device rd;
-			// create a random value for the seed
-			m_seed = rd();
-		}
-		// seed the generator
-		m_rng = generator(m_seed);
-	}
+    static constexpr seed_type default_seed = 0;
 
-	generator& rand() noexcept { return m_rng; }
+    static constexpr seed_type fixed_seed = generator::default_seed;
+
+    explicit RandomNumberGenerator(seed_type seed = default_seed) noexcept : m_seed(seed)
+    {
+        if (m_seed == default_seed)
+            m_seed = std::random_device{}();
+
+        m_rng = generator(m_seed);
+    }
+
+    generator& rand() noexcept { return m_rng; }
 };
 
 } // namespace engine
