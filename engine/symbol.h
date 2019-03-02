77197145a34a9e519a0a0a54efedad5b1fb2cd7b
Florian B <deamond4rk@gmail.com>
2019-03-02 19:56:37 +0100

    Symbol is an enum now

--- engine/symbol.h
+++ engine/symbol.h
@@ -3,13 +3,18 @@
 namespace engine
 {
 
-struct Symbol
+enum class Symbol : char
 {
-    static constexpr auto white  = 'O'; // white diamond "\xE2\x97\x86"
-    static constexpr auto black  = '@'; // black diamond "\xE2\x97\x87"
-    static constexpr auto square = '#'; // white diamond containing black small diamond "\xE2\x97\x88"
-    static constexpr auto split  = 'X'; // black diamond minus white x "\xE2\x9D\x96"
-    static constexpr auto dot    = '¤';  // white diamond with centred dot "\xE2\x9F\x90"
+    white  = 'O', // white diamond "\xE2\x97\x86"
+    black  = '@', // black diamond "\xE2\x97\x87"
+    square = '#', // white diamond containing black small diamond "\xE2\x97\x88"
+    split  = 'X', // black diamond minus white x "\xE2\x9D\x96"
+    dot    = '%', // white diamond with centred dot "\xE2\x9F\x90"
 };
+
+inline std::ostream & operator<<(std::ostream & out, Symbol s) noexcept
+{
+    return out << static_cast<char>(s);
+}
 
 } // namespace engine
