8e84dc435927ad97d7215dc645fa017f9eaa9483
Florian B <deamond4rk@gmail.com>
2019-03-02 19:56:51 +0100

    Clean wt app

--- ui/main.cpp
+++ ui/main.cpp
@@ -8,31 +8,31 @@
 class HelloApplication : public Wt::WApplication
 {
 public:
-    HelloApplication(const Wt::WEnvironment & _env);
+    HelloApplication(const Wt::WEnvironment & env);
 
 private:
-    Wt::WLineEdit * m_nameEdit;
-    Wt::WText * m_greeting;
+    Wt::WLineEdit * nameEdit;
+    Wt::WText * greeting;
 };
 
-HelloApplication::HelloApplication(const Wt::WEnvironment & _env)
-    : Wt::WApplication(_env)
+HelloApplication::HelloApplication(const Wt::WEnvironment & env)
+    : Wt::WApplication(env)
 {
     setTitle("Hello world");
 
     root()->addWidget(std::make_unique<Wt::WText>("Your name, please? "));
-    m_nameEdit = root()->addWidget(std::make_unique<Wt::WLineEdit>());
+    nameEdit = root()->addWidget(std::make_unique<Wt::WLineEdit>());
     Wt::WPushButton * button = root()->addWidget(std::make_unique<Wt::WPushButton>("Greet me."));
     root()->addWidget(std::make_unique<Wt::WBreak>());
-    m_greeting = root()->addWidget(std::make_unique<Wt::WText>());
+    greeting = root()->addWidget(std::make_unique<Wt::WText>());
     auto greet = [this]
     {
-        m_greeting->setText("Hello there, " + m_nameEdit->text());
+        greeting->setText("Hello there, " + nameEdit->text());
     };
     button->clicked().connect(greet);
 }
 
-int main(int argc, char **argv)
+int main(int argc, char ** argv)
 {
     return Wt::WRun(argc, argv, [](const Wt::WEnvironment & env)
     {
