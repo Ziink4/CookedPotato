e675f9eb2dbcd54b7a2265b6c17a97b6e6c85cfb
Ziink4 <deamond4rk@gmail.com>
2019-03-14 18:07:31 +0100

    Clean symbol enum

--- include/engine/symbol.h
+++ include/engine/symbol.h
@@ -1,20 +1,10 @@
 #pragma once
 
-namespace engine
+namespace engine::Symbol
 {
-
-enum class Symbol : char
-{
-    white  = 'O', // white diamond "\xE2\x97\x86"
-    black  = '@', // black diamond "\xE2\x97\x87"
-    square = '#', // white diamond containing black small diamond "\xE2\x97\x88"
-    split  = 'X', // black diamond minus white x "\xE2\x9D\x96"
-    dot    = '%', // white diamond with centred dot "\xE2\x9F\x90"
-};
-
-inline std::ostream & operator<<(std::ostream & out, Symbol s) noexcept
-{
-    return out << static_cast<char>(s);
-}
-
-} // namespace engine
+	constexpr char white = 'O';  // white diamond "\xE2\x97\x86"
+	constexpr char black = '@';  // black diamond "\xE2\x97\x87"
+	constexpr char square = '#'; // white diamond containing black small diamond "\xE2\x97\x88"
+	constexpr char split = 'X';  // black diamond minus white x "\xE2\x9D\x96"
+	constexpr char dot = '%';    // white diamond with centred dot "\xE2\x9F\x90"
+} // namespace engine::Symbol
