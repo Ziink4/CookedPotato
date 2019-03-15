ad2632360af431448f97346fc3bfd89ef72d71f4
Ziink4 <deamond4rk@gmail.com>
2019-03-15 18:17:28 +0100

    Added /EHsc on MSVC

--- cmake/compile_options.cmake
+++ cmake/compile_options.cmake
@@ -6,5 +6,5 @@ function(potato_setup_compile_options)
     # Enable warnings and C++17
     target_compile_options(${POTATO_FUNC_TARGET} PUBLIC $<$<CXX_COMPILER_ID:GNU>:-std=c++17;-Wall;-Wextra;-pedantic>)
     target_compile_options(${POTATO_FUNC_TARGET} PUBLIC $<$<CXX_COMPILER_ID:Clang>:-std=c++17;-Weverything;-Wno-c++98-compat;-Wno-c++98-compat-pedantic>)
-    target_compile_options(${POTATO_FUNC_TARGET} PUBLIC $<$<CXX_COMPILER_ID:MSVC>:/std:c++latest;/W4>)
+    target_compile_options(${POTATO_FUNC_TARGET} PUBLIC $<$<CXX_COMPILER_ID:MSVC>:/std:c++latest;/W4;/EHsc>)
 endfunction()
