099f5b8ba7b282759fee5e8343de7acc6a200e9a
Florian B <deamond4rk@gmail.com>
2019-04-07 17:13:44 +0200

    First draft of game loop test

---
+++ test/gameplay/game_flow.cpp
@@ -0,0 +1,90 @@
+#include <engine/rng.h>
+#include <engine/symbol.h>
+#include <engine/terrain.h>
+#include <engine/character.h>
+
+#include <gameplay/split_terrain.h>
+#include <gameplay/random_terrain.h>
+
+#include <gtest/gtest.h>
+using GoogleTestFixture = ::testing::Test;
+
+#include <iostream>
+
+std::ostream & print_spawn_points(std::ostream & out, const std::vector<std::vector<engine::point>> & spawns, const engine::Terrain & t) noexcept
+{
+    engine::point pt;
+
+    for (pt.y = 0; pt.y < t.height(); ++pt.y)
+    {
+        out << " ";
+
+        if (pt.y % 2 == 1) out << "  ";
+
+        for (pt.x = 0; pt.x < t.width(); ++pt.x)
+        {
+            const engine::Entity * const entity = t.at(pt);
+
+            if (entity)
+                entity->print_symbol(out);
+            else if (std::find(spawns[0].begin(), spawns[0].end(), pt) != spawns[0].end())
+                out << engine::symbol::square;
+            else if (std::find(spawns[1].begin(), spawns[1].end(), pt) != spawns[1].end())
+                out << engine::symbol::dot;
+            else
+                out << engine::symbol::black;
+
+            out << "   ";
+        }
+
+        out << "\n";
+    }
+
+    return out << "\n";
+}
+
+TEST(Game, BasicFlow)
+{
+    // Create rng
+    engine::random_number_engine rng{ engine::random_number_engine::default_seed };
+
+    // Create random terrain
+    engine::Terrain terrain = gameplay::generate_terrain(rng);
+
+    // Print generated terrain
+    std::cout << terrain << "\n";
+
+    // Get spawn positions
+    const auto spawn_points = gameplay::split_field(terrain, 2);
+
+    // Print nice spawn positions
+    print_spawn_points(std::cout, spawn_points, terrain);
+
+    // Select spawn points
+    std::array<engine::point, 2> selected_spawn_points;
+    std::sample(std::begin(spawn_points[0]), std::end(spawn_points[0]), std::begin(selected_spawn_points), 1, rng);
+    std::sample(std::begin(spawn_points[1]), std::end(spawn_points[1]), std::next(std::begin(selected_spawn_points)), 1, rng);
+    
+    // Print selected spawn points
+    std::cout << selected_spawn_points[0] << "\n";
+    std::cout << selected_spawn_points[1] << "\n";
+
+    // Create players
+    auto player_one = std::make_unique<engine::Character>(engine::Character::type::player);
+    auto player_two = std::make_unique<engine::Character>(engine::Character::type::player);
+
+    // Place players on the terrain
+    terrain.place_entity(move(player_one), selected_spawn_points[0]);
+    terrain.place_entity(move(player_two), selected_spawn_points[1]);
+
+    // Print terrain
+    std::cout << terrain << "\n";
+
+    // TODO : While one player is alive
+        // Run player one
+        // Check player two life
+        // Run player two
+        // Check player one life
+        // Update terrain
+    //
+}
